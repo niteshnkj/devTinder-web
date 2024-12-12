4bf2bcb909dd1722ba17137c4758cfe105b03ac3
nitesh <niteshkrjha.nkj@gmail.com>
2024-12-13 01:03:22 +0530

    basic setup && login form

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from "./components/Footer";
+import Login from "./components/Login";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Login />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
